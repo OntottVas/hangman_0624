8d41ff4345e623c6a8aebc5f1254da88a3a721ca
OntottVas <smithzero94@gmail.com>
2023-06-24 11:51:45 +0200

    hangman implementation started

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String result = "kecske";
+
+        Scanner scanner = new Scanner();
+
+
+
+        // beolvasni
+        // k -> YES
+        // o -> NO
+
+
+    }
+
+    public boolean characterCheck(String result) {
+        boolean win = false;
+        while (!win) {
+            System.out.println("Give me a letter.");
+            String character = scanner.nextLine();
+
+            boolean match = false;
+            for (int i = 0; i < result.length(); i++) {
+                if (result.charAt(i) == )
+            }
+
+        }
+    }
+}
